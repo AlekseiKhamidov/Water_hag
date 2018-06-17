aac191a49f3d6a4cac27792957e53d0c88a7cf2c
AlekseiKhamidov <aleksei.khamidov@gmail.com>
2018-06-17 18:43:52 +0500

    Added UpdateVKToken.php

---
+++ UpdateVKToken.php
@@ -0,0 +1,14 @@
+<?php
+	require_once 'config.php';
+
+	echo "<a href='https://oauth.vk.com/authorize?client_id=".
+          VK["application_id"].
+          "&redirect_uri=".
+          VK["base_domain"].
+          "&display=page&scope=".
+          VK["scopes"].
+          "&response_type=token&v=5.74'>
+          Получить токен
+        </a>";
+
+?>
