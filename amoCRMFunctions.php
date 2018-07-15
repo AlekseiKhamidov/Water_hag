5256fd0d9ba2de569e955d80b10794a45bf129ac
AlekseiKhamidov <aleksei.khamidov@gmail.com>
2018-07-15 20:43:29 +0500

    Added logger

--- amoCRMFunctions.php
+++ amoCRMFunctions.php
@@ -3,6 +3,25 @@
   require_once "config.php";
   require_once "functions.php";
 
+  use Monolog\Logger;
+  use Monolog\Formatter\JsonFormatter;
+  use Monolog\Handler\PhpConsoleHandler;
+  use Monolog\Handler\StreamHandler;
+
+  // Create the logger
+  $logger = new Logger('my_logger');
+  // Now add some handlers
+  $logger->pushHandler(new PHPConsoleHandler());
+
+
+  $formatter = new JsonFormatter();
+  // Create a handler
+  $stream = new StreamHandler(__DIR__.'/my_app.log', Logger::DEBUG);
+  $stream->setFormatter($formatter);
+  // bind it to a logger object
+  $logger->pushHandler($stream);
+
+
   try {
     $listener = new \AmoCRM\Webhooks\Listener();
 
@@ -21,7 +40,7 @@
   } catch (\AmoCRM\Exception $e) {
     $logger->error(sprintf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage()));
   }
-  
+
 
   function getContactsAndCompanies($json = false) {
     set_time_limit(0);  // global setting
