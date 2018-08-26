40c51beb1941d409003d084c2d5bce13c5e2f23f
hamidova-en <hamidova89@gmail.com>
2018-08-27 02:10:41 +0500

    add lk/index.php

---
+++ lk/getData.php
@@ -0,0 +1,14 @@
+<?php
+  require_once "../amoCRMFunctions.php";
+  require_once "../mysql.php";
+
+  $partnerId = getUserdata(
+    isset($_COOKIE['id']) ? intval($_COOKIE['id']) : 0
+    )['partner_id'];
+
+  $startdate = isset($_GET['start']) ? $_GET['start'] : strtotime('-1 month');
+  $enddate = isset($_GET['end']) ? $_GET['end'] : strtotime('now');
+  $leasing = isset($_GET['leasing']);
+
+  echo selectLeads($partnerId, $startdate, $enddate, $leasing);
+?>
