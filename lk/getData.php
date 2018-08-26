5e075cd198c27c691c849241a08e8d3ae9cb66e0
AlekseiKhamidov <aleksei.khamidov@gmail.com>
2018-08-26 19:30:19 +0500

    getData.php

--- lk/getData.php
+++ lk/getData.php
@@ -1,14 +1,13 @@
 <?php
-	require_once "../config.php";
-	require_once "mysql.php";
+  require_once "../amoCRMFunctions.php";
+  require_once "../mysql.php";
 
-//	print_r($_COOKIE['id']);
-//
-	$partnerId = getUserdata(intval($_COOKIE['id']))['partner_id'];
+  $partnerId = getUserdata(
+    isset($_COOKIE['id']) ? intval($_COOKIE['id']) : 0
+    )['partner_id'];
 
-	$startdate = isset($_GET['start']) ? $_GET['start'] : strtotime('-1 month');
-	$enddate = isset($_GET['end']) ? $_GET['end'] : strtotime('now');
-//$partnerId = getUserdata(intval($_COOKIE['id']));
-	// print_r($partnerId."!!!!!!!!!!!!!!!!!!!!!!!\n");
-	echo json_encode(selectLeads($partnerId, $startdate, $enddate));
+  $startdate = isset($_GET['start']) ? $_GET['start'] : strtotime('-1 month');
+  $enddate = isset($_GET['end']) ? $_GET['end'] : strtotime('now');
+
+  echo selectLeads($partnerId, $startdate, $enddate);
 ?>
